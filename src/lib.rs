49e20752c143ce2c60d56d201897d4bda65a7f2e
Martin Zeller <mz.bremerhaven@gmail.com>
2024-03-01 00:27:02 +0100

    Simple network packet and Nack handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,62 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use bytes::Bytes;
+use ndn_protocol::{Data, Interest};
+use ndn_tlv::{Tlv, TlvEncode};
+
+#[derive(Tlv, Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Packet {
+    Interest(Interest<Bytes>),
+    Data(Data<Bytes>),
+    LpPacket(LpPacket),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Tlv, Debug, Clone, PartialEq, Eq, Hash)]
+#[tlv(100)]
+pub struct LpPacket {
+    headers: Vec<LpHeader>,
+    fragment: Option<Fragment>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Tlv, Debug, Clone, PartialEq, Eq, Hash)]
+#[tlv(80)]
+pub struct Fragment {
+    data: Bytes,
+}
+
+#[derive(Tlv, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum LpHeader {
+    Nack(Nack),
+}
+
+#[derive(Tlv, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[tlv(800)]
+pub struct Nack {}
+
+impl Packet {
+    pub fn make_nack<T>(interest: Interest<T>) -> Self
+    where
+        T: TlvEncode,
+    {
+        Self::LpPacket(LpPacket {
+            headers: vec![LpHeader::Nack(Nack {})],
+            fragment: Some(Fragment {
+                data: interest.encode(),
+            }),
+        })
+    }
+}
+
+impl LpPacket {
+    pub fn is_nack(&self) -> bool {
+        for header in &self.headers {
+            #[allow(irrefutable_let_patterns)]
+            if let LpHeader::Nack(_) = header {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn fragment(&self) -> Option<Bytes> {
+        self.fragment.as_ref().map(|x| x.data.clone())
     }
 }
